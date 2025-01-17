77e5d19d7cfb7ef7e3a29779ae3b30f67d2c6690
iomckn <156865257+iomckn@users.noreply.github.com>
2025-01-17 23:32:52 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from distutils.core import setup
+setup(
+ name='quizz_game',
+ version='1.0',
+ author='{iomckn}',
+ license='MIT',
+ long_description=open('README.md').read(),
+)
